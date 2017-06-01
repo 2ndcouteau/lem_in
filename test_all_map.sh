606059b8a60f019e679b54d2fd9b7ef5c3ca7b14
yoko <yoko@Berenice>
2017-06-01 00:41:25 +0000

    UPDATE -- Add colors in test script

--- test_all_map.sh
+++ test_all_map.sh
@@ -1,8 +1,17 @@
+RED=$(printf "\e[31m")
+GREEN=$(printf "\e[32m")
+YELLOW=$(printf "\e[33m")
+BLUE=$(printf "\e[34m")
+MAGENTA=$(printf "\e[35m")
+CYAN=$(printf "\e[36m")
+WHITE=$(printf "\e[37m")
+RESET=$(printf "\e[m")
+
 for var in `ls maps/*`
 do
 	echo ""
-	echo "Test $var :"
+	echo "${BLUE}#${RESET}Test $var :"
 	./lem-in < $var 1>&-
 
-	[ `echo $?` == 0 ] && echo OK
+	[ `echo $?` == 0 ] && echo "${CYAN}SUCCESS${RESET}" || echo "${RED}ERROR${RESET}"
 done
