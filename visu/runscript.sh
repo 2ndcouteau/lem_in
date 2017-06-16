59a28a7ede34911053921422d731593201ab4489
yoko <yoko@Berenice>
2017-06-16 17:09:06 +0000

    FIX -- ERROR message if no path has been found UPDATE -- runscript has been improve for multiplatforme compatibility ADD -- a new map of 1k room

--- visu/runscript.sh
+++ visu/runscript.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 cd visu
 cat $1 | python3 visualize.py
-firefox vizualize.html
+
+if [ -e /usr/bin/iceweasel ]; then
+	iceweasel vizualize.html
+else if [ -e /usr/bin/chromium ]; then
+	chromium vizualize.html
+else if [ -e /usr/bin/firefox ]; then
+	firefox vizualize.html
+else
+	echo "WALLAH YA PAS DE NAVIGATEUR CONNU: \"navigateur vizualize.html\""
+fi
+fi
+fi
