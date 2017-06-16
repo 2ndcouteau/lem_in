476b87e98a356cae22d8e9f95fda6a35987c5e7b
yoko <yoko@Berenice>
2017-06-16 17:25:06 +0000

    UPDATE -- runscript.sh launch web browser in backend FIX -- map1k.map has now 2 good paths

--- visu/runscript.sh
+++ visu/runscript.sh
@@ -3,11 +3,11 @@ cd visu
 cat $1 | python3 visualize.py
 
 if [ -e /usr/bin/iceweasel ]; then
-	iceweasel vizualize.html
+	iceweasel vizualize.html &
 else if [ -e /usr/bin/chromium ]; then
-	chromium vizualize.html
+	chromium vizualize.html &
 else if [ -e /usr/bin/firefox ]; then
-	firefox vizualize.html
+	firefox vizualize.html &
 else
 	echo "WALLAH YA PAS DE NAVIGATEUR CONNU: \"navigateur vizualize.html\""
 fi
