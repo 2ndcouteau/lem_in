39012f10d9345d4f2f3d5c1d9ed5c9d5edcfdb10
yoko <yoko@Berenice>
2017-06-19 01:09:20 +0000

    UPDATE -- Big map are now skiped

--- test_all_map.sh
+++ test_all_map.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 RED=$(printf "\e[31m")
 GREEN=$(printf "\e[32m")
 YELLOW=$(printf "\e[33m")
@@ -7,11 +9,18 @@ CYAN=$(printf "\e[36m")
 WHITE=$(printf "\e[37m")
 RESET=$(printf "\e[m")
 
+if [ ! -e ./lem-in ]; then
+	make;
+fi
+
 for var in `ls maps/*`
 do
 	echo ""
 	echo "${BLUE}#${RESET}Test $var :"
-	./lem-in < $var 1>&-
-
-	[ `echo $?` == 0 ] && echo "${CYAN}SUCCESS${RESET}" || echo "${RED}ERROR${RESET}"
+	if [[ ! $var =~ ^maps/map1* ]] && [[ ! $var =~ ^maps/big* ]]; then
+		./lem-in < $var 1>&-
+		[ `echo $?` == 0 ] && echo "${CYAN}SUCCESS${RESET}" || echo "${RED}ERROR${RESET}"
+	else
+		echo "${MAGENTA}Test too long --> Skip${RESET}"
+	fi
 done
