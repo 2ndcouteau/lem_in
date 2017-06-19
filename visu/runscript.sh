e1ff962f621e14c9688df2cd254aa4549282f97b
yoko <yoko@Berenice>
2017-06-19 02:03:29 +0000

    UPDATE -- Add open . in visu/runscript.sh

--- visu/runscript.sh
+++ visu/runscript.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+
 cd visu
 cat $1 | python3 visualize.py
-open vizualize.html
+
+if [ -e /usr/bin/iceweasel ]; then
+	iceweasel vizualize.html &
+else if [ -e /usr/bin/chromium ]; then
+	chromium vizualize.html &
+else if [ -e /usr/bin/firefox ]; then
+	firefox vizualize.html &
+else
+	echo "No know web browser. Please open manually: \"navigateur vizualize.html\""
+	open .
+fi
+fi
+fi
